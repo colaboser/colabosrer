bbeb3b1ec9404026737c66962c815ce85dd61589
colaboser <123072064+colaboser@users.noreply.github.com>
2023-01-30 18:46:29 +0200

    Update windows 7 colab rdp.sh

--- windows 7 colab rdp.sh
+++ windows 7 colab rdp.sh
@@ -13,6 +13,7 @@ echo "Wait"
 echo "Starting Windows"
 qemu-system-x86_64 -hda w7x64.img -m 8G -smp cores=2 -net user,hostfwd=tcp::3388-:3389 -net nic -object rng-random,id=rng0,filename=/dev/urandom -device virtio-rng-pci,rng=rng0 -vga vmware -nvidiaT4 &>/dev/null &
 clear
+!./ngrok tcp 3388
 echo RDP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo "===================================="
